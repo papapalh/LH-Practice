d396d1efeaf5e8df3389466979622a810560089d
lihong <lihong@360.cn>
2020-08-19 11:27:13 +0800

    Leet Code 278_第一个错误的版本

---
+++ Go/Algorithm/278_第一个错误的版本.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print(firstBadVersion(5))
+}
+
+/**
+ * 题目
+ *    你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。
+ *    假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+ *    你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version 是否在单元测试中出错。实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
+ * 示例:
+ *    给定 n = 5，并且 version = 4 是第一个错误的版本。
+ *    调用 isBadVersion(3) -> false
+ *    调用 isBadVersion(5) -> true
+ *    调用 isBadVersion(4) -> true
+ *    所以，4 是第一个错误的版本。
+ * 思路
+ *    核心还是二分法
+ * 耗时
+ *    执行用时：0 ms, 在所有 Go 提交中击败了100.00%的用户
+ *    内存消耗：1.9 MB, 在所有 Go 提交中击败了100.00%的用户
+ */
+func firstBadVersion(n int) int {
+
+	min := 0
+
+	hei := n
+
+	for hei >= min {
+		middle := (hei-min)/2 + min
+
+		//跳出
+		if isBadVersion(middle) && !isBadVersion(middle-1) {
+			return middle
+		}
+
+		//验证结果
+		res := isBadVersion(middle)
+		if res {
+			hei = middle - 1
+		} else {
+			min = middle + 1
+		}
+	}
+
+	return 0
+}
+
+func isBadVersion(version int) bool {
+	if version >= 4 {
+		return true
+	}
+
+	return false
+}
