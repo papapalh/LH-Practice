a2732b0ad1e29d7a449f9772316ad33b92512e4f
lihong <lihong@360.cn>
2020-10-10 11:33:59 +0800

    235_二叉搜索树的最近公共祖先

--- Go/Algorithm/235_二叉搜索树的最近公共祖先.go
+++ Go/Algorithm/235_二叉搜索树的最近公共祖先.go
@@ -26,7 +26,7 @@ func main() {
 }
 
 /**
- * 思路
+ * 思路1
  *    两次遍历，获取节点路径，取交点
  * 耗时
  *    执行用时：24 ms, 在所有 Go 提交中击败了85.04%的用户
@@ -74,4 +74,26 @@ func getPath(root, point *TreeNode) []*TreeNode {
 	}
 
 	return res
+}
+
+/**
+ * 思路2
+ *    在方法一中，我们对从根节点开始，通过遍历找出到达节点 pp 和 qq 的路径，一共需要两次遍历。我们也可以考虑将这两个节点放在一起遍历。
+ *    整体的遍历过程与方法一中的类似：
+ *    我们从根节点开始遍历；
+ *        如果当前节点的值大于 pp 和 qq 的值，说明 pp 和 qq 应该在当前节点的左子树，因此将当前节点移动到它的左子节点；
+ *        如果当前节点的值小于 pp 和 qq 的值，说明 pp 和 qq 应该在当前节点的右子树，因此将当前节点移动到它的右子节点；
+ *        如果当前节点的值不满足上述两条要求，那么说明当前节点就是「分岔点」。此时，pp 和 qq 要么在当前节点的不同的子树中，要么其中一个就是当前节点。
+ */
+func lowestCommonAncestor2(root, p, q *TreeNode) (ancestor *TreeNode) {
+	ancestor = root
+	for {
+		if p.Val < ancestor.Val && q.Val < ancestor.Val {
+			ancestor = ancestor.Left
+		} else if p.Val > ancestor.Val && q.Val > ancestor.Val {
+			ancestor = ancestor.Right
+		} else {
+			return
+		}
+	}
 }
